e932963f7b6c8d67536c463f4c98fe2b9e242fc8
Seal <cealll>
2024-07-19 14:15:04 +0800

    July 19 | Fixed Page Icons

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/x-icon" href="{{asset('logo\sllogo.png')}}">
+        <link rel="icon" type="image/x-icon" href="{{asset('assets\sllogo.png')}}">
         @hasSection('title')
 
             <title>@yield('title') - {{ config('app.name') }}</title>
