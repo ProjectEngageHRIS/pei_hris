db149127a607eb7dcf23adb5afd98dd742b7731b
Rein <97869210+paulineregina@users.noreply.github.com>
2024-07-22 17:45:31 +0800

    Assigned Task Table

--- resources/views/livewire/mytasks/my-tasks-table.blade.php
+++ resources/views/livewire/mytasks/my-tasks-table.blade.php
@@ -226,7 +226,7 @@
                                         {{$my_task->assigned_task}}
                                     </td>
                                     <td class="px-6 py-4 text-center whitespace-nowrap" colspan="3">
-                                        <span  class="inline-flex items-center px-2 py-1 text-xs font-medium text-center text-green-900 rounded-lg bg-customGreen me-2 ">
+                                        <span  class="inline-flex items-center px-2 py-1 text-xs font-medium text-center text-white rounded-lg bg-customGreen me-2 ">
                                             <svg class="w-6 h-6 text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
                                             </svg>
