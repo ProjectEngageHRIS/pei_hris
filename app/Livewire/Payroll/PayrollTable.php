05a293ebc3e02b83c79347bb72bd037049dad6e5
cealll <sealfajardo0706@gmail.com>
2024-07-24 20:36:42 +0800

    July 24 | Added Payroll Table Filters

--- app/Livewire/Payroll/PayrollTable.php
+++ app/Livewire/Payroll/PayrollTable.php
@@ -20,6 +20,16 @@ class PayrollTable extends Component
 
     public $resultsCount;
 
+    public $phaseFilter;
+
+    public $phaseFilterName;
+    public $monthFilter;
+    
+    public $monthFilterName;
+    public $yearFilter;
+
+    public $yearFilterName;
+
 
     public function search()
     {
@@ -42,34 +52,187 @@ class PayrollTable extends Component
         $loggedInUser = auth()->user();
 
         $query = Payroll::where('target_employee', $loggedInUser->employee_id)->select('target_employee', 'month', 'phase', 'year');
-        // switch ($this->filter) {
-        //     case '1':
-        //         $query->whereDate('start_date',  Carbon::today());
-        //         $this->filterName = "Today";
-        //         break;
-        //     case '2':
-        //         $query->whereBetween('start_date', [Carbon::today()->startOfWeek(), Carbon::today()]);
-        //         $this->filterName = "Last 7 Days";
-        //         break;
-        //     case '3':
-        //         $query->whereBetween('start_date', [Carbon::today()->subDays(30), Carbon::today()]);
-        //         // $query->whereDate('start_date', '>=', Carbon::today()->subDays(30), '<=', Carbon::today());
-        //         $this->filterName = "Last 30 days";
-        //         break;
-        //     case '4':
-        //         $query->whereBetween('start_date', [Carbon::today()->subMonths(6), Carbon::today()]);
-        //         // $query->whereDate('start_date', '>=', Carbon::today()->subMonths(6), '<=', Carbon::today());
-        //         $this->filterName = "Last 6 Months";
-        //         break;
-        //     case '5':
-        //         $query->whereBetween('start_date', [Carbon::today()->subYear(), Carbon::today()]);
-        //         // $query->whereDate('start_date', '>=', Carbon::today()->subYear(), '<=', Carbon::today());
-        //         $this->filterName = "Last Year";
-        //         break;
-        //     default:
-        //         $this->filterName = "All"; 
-        //         break;
-        // }
+        
+        
+        switch ($this->phaseFilter) {
+            case '1':
+                $query->where('phase', '1st Half');
+                $this->phaseFilterName = "1st Half";
+                break;
+            case '2':
+                $query->where('phase', '2nd Half');
+                $this->phaseFilterName = "2nd Half";
+                break;
+            default:
+                $this->phaseFilterName = "All";
+                break;
+        }
+
+        switch ($this->monthFilter) {
+            case '0':
+                $query->where('month', 'January');
+                $this->monthFilterName = "January";
+                break;
+            case '1':
+                $query->where('month', 'February');
+                $this->monthFilterName = "February";
+                break;
+            case '2':
+                $query->where('month', 'March');
+                $this->monthFilterName = "March";
+                break;
+            case '3':
+                $query->where('month', 'April');
+                $this->monthFilterName = "April";
+                break;
+            case '4':
+                $query->where('month', 'May');
+                $this->monthFilterName = "May";
+                break;
+            case '5':
+                $query->where('month', 'June');
+                $this->monthFilterName = "June";
+                break;
+            case '6':
+                $query->where('month', 'July');
+                $this->monthFilterName = "July";
+                break;
+            case '7':
+                $query->where('month', 'August');
+                $this->monthFilterName = "August";
+                break;
+            case '8':
+                $query->where('month', 'September');
+                $this->monthFilterName = "September";
+                break;
+            case '9':
+                $query->where('month', 'October');
+                $this->monthFilterName = "October";
+                break;
+            case '10':
+                $query->where('month', 'November');
+                $this->monthFilterName = "November";
+                break;
+            case '11':
+                $query->where('month', 'December');
+                $this->monthFilterName = "December";
+                break;
+            default:
+                $this->monthFilterName = "All";
+                break;
+        }
+
+        switch ($this->yearFilter) {
+            case '2024':
+                $query->where('year', 2024);
+                $this->yearFilterName = "2024";
+                break;
+            case '2023':
+                $query->where('year', 2023);
+                $this->yearFilterName = "2023";
+                break;
+            case '2022':
+                $query->where('year', 2022);
+                $this->yearFilterName = "2022";
+                break;
+            case '2021':
+                $query->where('year', 2021);
+                $this->yearFilterName = "2021";
+                break;
+            case '2020':
+                $query->where('year', 2020);
+                $this->yearFilterName = "2020";
+                break;
+            case '2019':
+                $query->where('year', 2019);
+                $this->yearFilterName = "2019";
+                break;
+            case '2018':
+                $query->where('year', 2018);
+                $this->yearFilterName = "2018";
+                break;
+            case '2017':
+                $query->where('year', 2017);
+                $this->yearFilterName = "2017";
+                break;
+            case '2016':
+                $query->where('year', 2016);
+                $this->yearFilterName = "2016";
+                break;
+            case '2015':
+                $query->where('year', 2015);
+                $this->yearFilterName = "2015";
+                break;
+            case '2014':
+                $query->where('year', 2014);
+                $this->yearFilterName = "2014";
+                break;
+            case '2013':
+                $query->where('year', 2013);
+                $this->yearFilterName = "2013";
+                break;
+            case '2012':
+                $query->where('year', 2012);
+                $this->yearFilterName = "2012";
+                break;
+            case '2011':
+                $query->where('year', 2011);
+                $this->yearFilterName = "2011";
+                break;
+            case '2010':
+                $query->where('year', 2010);
+                $this->yearFilterName = "2010";
+                break;
+            case '2009':
+                $query->where('year', 2009);
+                $this->yearFilterName = "2009";
+                break;
+            case '2008':
+                $query->where('year', 2008);
+                $this->yearFilterName = "2008";
+                break;
+            case '2007':
+                $query->where('year', 2007);
+                $this->yearFilterName = "2007";
+                break;
+            case '2006':
+                $query->where('year', 2006);
+                $this->yearFilterName = "2006";
+                break;
+            case '2005':
+                $query->where('year', 2005);
+                $this->yearFilterName = "2005";
+                break;
+            case '2004':
+                $query->where('year', 2004);
+                $this->yearFilterName = "2004";
+                break;
+            case '2003':
+                $query->where('year', 2003);
+                $this->yearFilterName = "2003";
+                break;
+            case '2002':
+                $query->where('year', 2002);
+                $this->yearFilterName = "2002";
+                break;
+            case '2001':
+                $query->where('year', 2001);
+                $this->yearFilterName = "2001";
+                break;
+            case '2000':
+                $query->where('year', 2000);
+                $this->yearFilterName = "2000";
+                break;
+            case '1999':
+                $query->where('year', 1999);
+                $this->yearFilterName = "1999";
+                break;
+            default:
+                $this->yearFilterName = "All";
+                break;
+        }
+        
+        
 
         if (strlen($this->search) >= 1) {
             $query->where('month', 'like', '%' . $this->search . '%');
