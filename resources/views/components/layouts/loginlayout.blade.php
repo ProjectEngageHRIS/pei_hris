4982a9062e44004363ad0fc2de18c9f7e1632433
uzuriel <seeaanlb@gmail.com>
2024-07-29 15:01:46 +0800

    Login Layout: Renamed Dashboard from Employee Portal to Portals Dashboard

--- resources/views/components/layouts/loginlayout.blade.php
+++ resources/views/components/layouts/loginlayout.blade.php
@@ -13,7 +13,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
     <link rel="icon" type="image/x-icon" href="{{asset('logo\sllogo.png')}}">
-    <title>Employee Portal</title>
+    <title>Portals Dashboard</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css"> --}}
     {{-- <title>Document</title> --}}
@@ -62,7 +62,7 @@
         /* width: auto; */
         /* max-width: 600px; */
 
-        height: 200px;  
+        height: 200px;
         margin-top: -200px;
         margin-left: -280px;
         border-radius: 8px;
@@ -84,7 +84,7 @@
                 {{-- <div class="p-4"> --}}
                     @yield('content')
                 {{-- </div> --}}
-    
+
     {{-- <script>
         // Get the toggle button
         const toggleButton = document.getElementById('toggleSidebar');
@@ -136,7 +136,7 @@
         const logoSidebar = document.getElementById('logo-sidebar');
         // Initialize a flag to track the first click
         let firstClick = true;
-      
+
         // Define the SVG icons
         const closeIcon = `<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                             <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h14M1 6h14M1 11h7"/>
@@ -144,10 +144,10 @@
         const openIcon = `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
                           </svg>`;
-      
+
         // Set the initial icon
         toggleButton.innerHTML = closeIcon;
-      
+
         // Toggle dropdown visibility and content padding when the button is clicked
         toggleButton.addEventListener('click', function(event) {
           // If it's the first click, do nothing
@@ -155,7 +155,7 @@
             firstClick = false;
             return;
           }
-      
+
           // Toggle dropdown visibility
           const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
           toggleButton.setAttribute('aria-expanded', String(!isExpanded));
@@ -165,29 +165,29 @@
           } else {
             logoSidebar.style.display = '';
           }
-      
+
           // Toggle content padding
           const mainContent = document.getElementById('padding-content');
           mainContent.classList.toggle('sm:ml-64');
-      
+
           // Change the icon
           toggleButton.innerHTML = isExpanded ? openIcon : closeIcon;
-      
+
           // Prevent event propagation to avoid closing the sidebar
           event.stopPropagation();
         });
-      
+
         // Hide the dropdown by default on screens narrower than 640 pixels
         if (window.innerWidth <= 640) {
           logoSidebar.classList.add('hidden');
         }
     </script>
-      
+
 @endsection
 
 
 
 
-    
+
 </body>
 </html>
