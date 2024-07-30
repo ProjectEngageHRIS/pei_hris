51a54bc1e19c04db53930d8833cf5152a208aea5
Seal <cealll>
2024-07-30 18:16:07 +0800

    July 30 | Added Loading Prompt

--- resources/views/livewire/activities/activities-gallery.blade.php
+++ resources/views/livewire/activities/activities-gallery.blade.php
@@ -134,16 +134,11 @@
                                                         <label for="poster" style="height: 121px;"  class="relative p-1 flex flex-col items-center  border-2 border-gray-400 border-dashed justify-center w-full h-32 rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                                         @if($data->poster && $removedImage != True)
                                                                 @if($poster)
-                                                                    {{-- @php
-                                                                        $data->poster = $this->getPoster();
-                                                                    @endphp --}}
                                                                     <img src="{{ $poster->temporaryUrl() }}" class="w-full h-full object-contain" alt="Uploaded Image">
                                                                 @elseif(is_string($data->poster))
                                                                     <img src="{{ asset('storage/'. $data->poster) }}" alt="Image Description" class="w-full h-full object-contain"> 
-
                                                                 @endif
                                                                 <input id="poster" type="file" class="hidden" wire:model.live="poster">
-                                    
                                                                 <button type="button" wire:click="removeImage" class="absolute top-0 right-0 m-2 text-red-600 py-1  rounded">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -397,7 +392,7 @@
                                                 
                                             </div>
                                             {{-- <hr class="border-gray-700"> --}}
-                                            <button  type="submit" class="w-full text-white bg-customRed hover:bg-red-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Edit Announcement</button>
+                                            <button @click="openEditForm = false"  type="submit" class="w-full text-white bg-customRed hover:bg-red-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Edit Announcement</button>
                                                                         
 
                                             
@@ -433,8 +428,8 @@
     </div>
 
             <!-- Loading screen -->
-            <div wire:loading wire:target="deleteActivity, editActivity " class="load-over z-50">
-                <div wire:loading wire:target="deleteActivity, editActivity " class="loading-overlay z-50">
+            <div wire:loading wire:target="deleteActivity, editActivity, editAnnouncement" class="load-over z-50">
+                <div wire:loading wire:target="deleteActivity, editActivity, editAnnouncement " class="loading-overlay z-50">
                     <div class="flex flex-col justify-center items-center">
                         <div class="spinner"></div>
                         <p>Updating...</p>
