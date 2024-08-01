4d17a04eee5df680624c00fdb3eb925afd948413
uzuriel <seeaanlb@gmail.com>
2024-08-01 13:40:13 +0800

    Login Dashboard: Changed icon for IT Portal

--- resources/views/livewire/dashboard/login-dashboard.blade.php
+++ resources/views/livewire/dashboard/login-dashboard.blade.php
@@ -1,6 +1,6 @@
-<main class="h-full flex justify-center p-36">
-    <div class="flex flex-row justify-center items-center gap-4 p-8 rounded-lg">
-        <button wire:click.prevent="employeePortal" class="bg-navButton text-customRed flex flex-col justify-center items-center p-4 size-40 rounded-lg gap-2 shadow hover:text-white hover:bg-customRed">
+<main class="flex justify-center h-full p-36">
+    <div class="flex flex-row items-center justify-center gap-4 p-8 rounded-lg">
+        <button wire:click.prevent="employeePortal" class="flex flex-col items-center justify-center gap-2 p-4 rounded-lg shadow bg-navButton text-customRed size-40 hover:text-white hover:bg-customRed">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
             </svg>
@@ -11,23 +11,23 @@
             @endif
         </button>
         {{-- @if ($is_hr) --}}
-        <button wire:click="humanResourcePortal" class="bg-navButton text-customRed flex flex-col justify-center items-center p-4 size-40 rounded-lg gap-2 shadow hover:text-white hover:bg-customRed">
+        <button wire:click="humanResourcePortal" class="flex flex-col items-center justify-center gap-2 p-4 rounded-lg shadow bg-navButton text-customRed size-40 hover:text-white hover:bg-customRed">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
             </svg> HR Portal
         </button>
         {{-- @endif --}}
         {{-- @if ($is_accounting) --}}
-        <button wire:click="accountingPortal" class="bg-navButton text-customRed flex flex-col justify-center items-center p-4 size-40 rounded-lg gap-2 shadow hover:text-white hover:bg-customRed">
+        <button wire:click="accountingPortal" class="flex flex-col items-center justify-center gap-2 p-4 rounded-lg shadow bg-navButton text-customRed size-40 hover:text-white hover:bg-customRed">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
             </svg> Accounting Portal
         </button>
         {{-- @endif --}}
         {{-- @if ($is_it) --}}
-        <button wire:click="itPortal" class="bg-navButton text-customRed flex flex-col justify-center items-center p-4 size-40 rounded-lg gap-2 shadow hover:text-white hover:bg-customRed">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+        <button wire:click="itPortal" class="flex flex-col items-center justify-center gap-2 p-4 rounded-lg shadow bg-navButton text-customRed size-40 hover:text-white hover:bg-customRed">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
             </svg> IT Portal
         </button>
         {{-- @endif --}}
