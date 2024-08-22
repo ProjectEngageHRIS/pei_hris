f8f481f0ea63d1039421c12970f657d2d716a9d8
Seal <cealll>
2024-08-22 18:03:21 +0800

    August 22 | Added Little Fix on Deactivate and Delete Buttons

--- resources/views/livewire/dashboard/hr-dashboard-view.blade.php
+++ resources/views/livewire/dashboard/hr-dashboard-view.blade.php
@@ -1079,16 +1079,16 @@
                                             </button>
                                             
                                             <!-- Deactivate Button -->
-                                            <button @click="openDeactivateModal('{{ $employee->employee_id }}')"
-                                                    type="button"
-                                                    class="flex items-center py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-red-700 rounded-lg">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 mr-3">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
-                                                </svg>
-                                                Deactivate
-                                            </button>
-
-                                            @if($employee->active != 1)
+                                            @if($employee->active == 1)
+                                                <button @click="openDeactivateModal('{{ $employee->employee_id }}')"
+                                                        type="button"
+                                                        class="flex items-center py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-red-700 rounded-lg">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 mr-3">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                                                    </svg>
+                                                    Deactivate
+                                                </button>
+                                            @else
                                                 <!-- Delete Button -->
                                                 <button @click="openDeleteModal('{{ $employee->employee_id }}')"
                                                         type="button"
