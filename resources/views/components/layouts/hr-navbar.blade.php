faa2a9ad6bd77669fb5b0832f8550d66ee7be70e
uzuriel <seeaanlb@gmail.com>
2024-08-28 15:33:11 +0800

    HR Portal Responsiveness

--- resources/views/components/layouts/hr-navbar.blade.php
+++ resources/views/components/layouts/hr-navbar.blade.php
@@ -9,8 +9,11 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <link rel="icon" type="image/x-icon" href="{{asset('assets\sllogo.png')}}">
         <title>HR Portal</title>
+        <title>HR Portal</title>
         @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
+    <body class="flex flex-col h-full min-h-screen bg-gray-200">
+        @extends('components.layouts.base')
     <body class="flex flex-col h-full min-h-screen bg-gray-200">
         @extends('components.layouts.base')
 
@@ -19,9 +22,22 @@
                 @livewire('sidebar.hr-sidebar-view')
             </header>
             <main class="p-8 min-w-[350px] w-full">
+                <div wire:offline>
+                    <div class="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        <span class="sr-only">Info</span>
+                        <div>
+                            <span class="font-medium">Warning alert!</span> You are offline. Check Your Internet Connection.
+                        </div>
+                    </div>
+                </div>
                 @isset($slot)
                     {{ $slot }}
                 @endisset
+            </main>
+        @endsection
             </main>
         @endsection
     </body>
