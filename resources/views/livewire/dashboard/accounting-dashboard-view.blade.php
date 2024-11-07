0b3f1e95776bfd683ccd9df874159ab000cc663c
cealll <sealfajardo0706@gmail.com>
2024-11-07 23:36:24 +0800

    Nov 7, 2024 | Fixed Rerouting and removed Employee Id from accounting

--- resources/views/livewire/dashboard/accounting-dashboard-view.blade.php
+++ resources/views/livewire/dashboard/accounting-dashboard-view.blade.php
@@ -776,9 +776,6 @@
                                                         <path d="M11.013 2.513a1.75 1.75 0 0 1 2.475 2.474L6.226 12.25a2.751 2.751 0 0 1-.892.596l-2.047.848a.75.75 0 0 1-.98-.98l.848-2.047a2.75 2.75 0 0 1 .596-.892l7.262-7.261Z"></path>
                                                     </svg>
                                                 </button>
-                                                <div x-data>
-                                                    <p>Employee ID: <span x-text="$store.editAccount.currentEditModal.employeeID"></span></p>
-                                                </div>
                                                 <!-- Main modal -->
                                                 <div x-show="$store.editAccount.openPayrollEditAccountModal && $store.editAccount.currentEditModal === '{{ $loop->index }}'" class="fixed inset-0 z-50 flex items-center justify-center">
                                                     <!-- Backdrop -->
